9b5dd43a923a5ff1cf8f9c4a87da97158cad8e4b
Ian <ianferiksson@gmail.com>
2022-03-25 14:27:04 +0100

    Design

--- javascript.js
+++ javascript.js
@@ -7,14 +7,16 @@ async function weather() {
   let currentweatherjson = await curentweather.json();
   console.log(currentweatherjson);
   let temprature = currentweatherjson.current.temp_c;
+  let city = currentweatherjson.location.name;
   let Last_updated = currentweatherjson.current.last_updated;
   let feelslike_c = currentweatherjson.current.feelslike_c;
   let gust_kph = currentweatherjson.current.gust_kph;
   let weather = document.getElementById("weather");
-  weather.innerHTML += `<ul>Last updated: ${Last_updated}</ul>`;
-  weather.innerHTML += `<ul>Temprature: ${temprature}°C </ul>`;
-  weather.innerHTML += `<ul>It feelslike: ${feelslike_c}°C </ul>`;
-  weather.innerHTML += `<ul>Wind speeds: ${gust_kph}km/h </ul>`;
+  weather.innerHTML += `<td>${city}</td>`;
+  weather.innerHTML += `<td>${temprature}°C </td>`;
+  weather.innerHTML += `<td>${feelslike_c}°C </td>`;
+  weather.innerHTML += `<td>${gust_kph}km/h </td>`;
+  weather.innerHTML += `<td>${Last_updated}</td>`;
 }
 
 weather();
