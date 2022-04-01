7a20308ed1f84f4575bdfc7e8f50e54d58cbf673
Ian <ianferiksson@gmail.com>
2022-04-01 15:21:04 +0200

    2022-04-01

--- javascript.js
+++ javascript.js
@@ -1,22 +1,29 @@
 const key = "691b87c3afa544e5801120929221803";
 const url = "http://api.weatherapi.com/v1";
-async function weather() {
-  let curentweather = await fetch(
-    "http://api.weatherapi.com/v1/current.json?key=" + key + "&q=stockholm"
+let search = document.getElementById("search");
+
+async function getWeather() {
+  var curentweather = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=${key}&q=${search.value}`
   );
-  let currentweatherjson = await curentweather.json();
+  var currentweatherjson = await curentweather.json();
   console.log(currentweatherjson);
-  let temprature = currentweatherjson.current.temp_c;
-  let city = currentweatherjson.location.name;
-  let Last_updated = currentweatherjson.current.last_updated;
-  let feelslike_c = currentweatherjson.current.feelslike_c;
-  let gust_kph = currentweatherjson.current.gust_kph;
-  let weather = document.getElementById("weather");
-  weather.innerHTML += `<td>${city}</td>`;
-  weather.innerHTML += `<td>${temprature}°C </td>`;
-  weather.innerHTML += `<td>${feelslike_c}°C </td>`;
-  weather.innerHTML += `<td>${gust_kph}km/h </td>`;
-  weather.innerHTML += `<td>${Last_updated}</td>`;
+  return currentweatherjson;
 }
 
-weather();
+search.onkeydown = async function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    let currentweatherjson = await getWeather();
+    let temprature = currentweatherjson.current.temp_c;
+    let city = currentweatherjson.location.name;
+    let Last_updated = currentweatherjson.current.last_updated;
+    let feelslike_c = currentweatherjson.current.feelslike_c;
+    let gust_kph = currentweatherjson.current.gust_kph;
+    let weather = document.getElementById("Table");
+    weather.insertAdjacentHTML(
+      "beforeend",
+      `<tr><td>${city}</td><td>${temprature}°C </td><td>${feelslike_c}°C </td><td>${gust_kph}km/h </td><td>${Last_updated}</td></tr>`
+    );
+  }
+};
